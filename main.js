4d139b58ad045552414a8fc75601d11928875396
M-3mad17 <quwemohamed.emad2020@gmail.com>
2025-09-03 22:07:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+// document.getElementById("demo").innerHTML = "heloooooooo";
+// document.getElementById("demo").style.textAlign = 'center';
+// document.getElementById("demo").style.backgroundColor = 'red';
+
+
+// document.getElementById('ahmed').onclick = function(){
+//     document.getElementById('result').innerHTML = Date()
+// }
+
+
+// console.log(x)
+// console.log('jjjjjjjjjjj')
